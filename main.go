11b6ba475c1baedf596253b893f7765e8fc17c4d
David Davies <d.davies.work@gmail.com>
2024-11-18 21:06:42 -0500

    Add JSON file handling and refactor item ID validation

--- main.go
+++ main.go
@@ -19,6 +19,31 @@ func remove(id int) {
 }
 
 func main() {
+	//make json file if file doesnt exist
+	_, err := os.Stat("items.json")
+	if os.IsNotExist(err) {
+		file, err := os.Create("items.json")
+		if err != nil {
+			fmt.Println("Error creating file: ", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+	}
+
+	//file open
+	file, err := os.OpenFile("items.json", os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	
+
+
+
+
+
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a command")
 		os.Exit(1)
@@ -28,32 +53,31 @@ func main() {
 
 	switch cmd {
 	case "add":
-		if item == "" {
-			fmt.Println("Please provide item")
-			os.Exit(1)
-		}
+		missingItemCheck(item)
 		add(item)
 	case "update":
-		if item == "" {
-			fmt.Println("Please provide item id")
-			os.Exit(1)
-		}
-		itemID, err := strconv.Atoi(item)
-		if err != nil {
-			fmt.Println("Please provide a valid item id")
-			os.Exit(1)
-		}
+		missingItemCheck(item)
+		itemID := checkValidItemId(item)
 		update(itemID)
 	case "remove":
-		if item == "" {
-			fmt.Println("Please provide item id")
-			os.Exit(1)
-		}
-		itemID, err := strconv.Atoi(item)
-		if err != nil {
-			fmt.Println("Please provide a valid item id")
-			os.Exit(1)
-		}
+		missingItemCheck(item)
+		itemID := checkValidItemId(item)
 		remove(itemID)
+	}
+}
+
+func checkValidItemId(item string) int {
+	itemID, err := strconv.Atoi(item)
+	if err != nil {
+		fmt.Println("Please provide a valid item id")
+		os.Exit(1)
+	}
+	return itemID
+}
+
+func missingItemCheck(item string) {
+	if item == "" {
+		fmt.Println("Please provide item")
+		os.Exit(1)
 	}
 }	
